665b045bac626dd92837269a32460af18849a2cc
Mike Babb <mike7400@gmail.com>
2022-07-28 23:24:29 -0400

    Comments

--- src/generate_points.py
+++ src/generate_points.py
@@ -12,10 +12,10 @@ import numpy as np
 from scipy.spatial import KDTree, distance
 from sklearn.metrics import pairwise_distances
 
-from vexcode import pen, drivetrain, PREAMBLE, POSTAMBLE, PLAYGROUND_SIZE
+from vexcode import PLAYGROUND_SIZE, POSTAMBLE, PREAMBLE, drivetrain, pen
 
 
-def scale_to_aspect_ratio(n, aspect_ratio):
+def scale_to_aspect_ratio(n: int, aspect_ratio: float):
     if aspect_ratio <= 1:
         return n, int(n * aspect_ratio)
     else:
@@ -27,7 +27,7 @@ def make_direction_vector(angle: float):
     return np.array([np.cos(angle), np.sin(angle)])
 
 
-def linear_map(t: float, x1: float, y1: float, x2=0.0, y2=1.0):
+def linear_map(t: float, x1: float, y1: float, x2: float = 0.0, y2: float = 1.0):
     a = (x2 - y2) / (x1 - y1)
     b = x2 - (x2 - y2) / (x1 - y1) * x1
     return a * t + b
@@ -70,7 +70,7 @@ def scale(sx: float, sy: float):
     return S
 
 
-def rotate(degrees=0):
+def rotate(degrees: float = 0):
     angle = np.deg2rad(degrees)
 
     R = np.array(
@@ -102,7 +102,7 @@ def center(arr: np.ndarray):
     return translate(-dx / 2, -dy / 2)
 
 
-def masked_argmin(arr: np.ndarray, mask: np.ma.array):
+def masked_argmin(arr: np.ndarray, mask: np.ndarray):
     masked = np.ma.array(arr, mask=mask)
     return masked.argmin()
 
@@ -134,8 +134,8 @@ def order_points(
     return points[ixs], np.asarray(distances)
 
 
-def load_image(path, size=None):
-    im = cv2.imread(path)
+def load_image(image_path: str, size=None):
+    im = cv2.imread(image_path)
     im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
     if size is not None:
@@ -144,11 +144,12 @@ def load_image(path, size=None):
     return im
 
 
-def find_contours(im, threshold1=150, threshold2=255):
+def find_contours(im: np.ndarray, threshold1: int = 150, threshold2: int = 255):
     im = cv2.medianBlur(im, 1)
     im = cv2.Canny(im, threshold1, threshold2)
     ret, im = cv2.threshold(im, threshold1, threshold2, 0)
 
+    # Good params for findContours
     contours, _ = cv2.findContours(im, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     contours = [np.squeeze(i, axis=1) for i in contours]
 
@@ -156,9 +157,15 @@ def find_contours(im, threshold1=150, threshold2=255):
 
 
 def make_traceable_path(points: np.ndarray):
+    """Trace out a path within the points array, getting the distance and angle
+    between each consecutive pair of points, e.g:
+
+        points = p1, p2, p3 ... p_n
+        path = (p1, p2), (p2, p3) ... (p_n - 1, p_n)
+    """
     x_min, x_max = np.min(points[..., 0]), np.max(points[..., 0])
 
-    path = []
+    image_path = []
 
     prev_p = make_direction_vector(0)
 
@@ -179,20 +186,25 @@ def make_traceable_path(points: np.ndarray):
             "p": p,
             "color": color,
         }
-        path.append(path_i)
+        image_path.append(path_i)
 
         prev_p = p
 
-    return path
+    return image_path
 
 
 def generate_code(
-    points: np.ndarray, distances: np.ndarray, mode: Literal["all", "path"] = "path"
+    points: np.ndarray,
+    distances: np.ndarray,
+    mode: Literal["all", "image_path"] = "image_path",
 ) -> str:
+    """Generates the actual code for the robot.
+    Bulk of the work is done in make_traceable_path,
+    though we optimize the number or drives, turns, and color changes."""
     if mode == "all":
         return f"IMAGE_POINTS = {points.tolist()}"
-    elif mode == "path":
-        path = make_traceable_path(points)
+    elif mode == "image_path":
+        image_path = make_traceable_path(points)
 
         avg_dist = np.average(distances)
 
@@ -203,7 +215,7 @@ def generate_code(
 
         thin = None
 
-        for n, p in enumerate(path):
+        for n, p in enumerate(image_path):
             angle, distance, color = p["angle"], p["distance"], p["color"]
 
             if prev_angle != angle:  # optimizing consecutive same-angle turns
@@ -240,72 +252,61 @@ def generate_image():
 {POSTAMBLE}"""
 
 
-# path = "assets/1000_F_106135417_llsURwzfzT2aFy7mhWQ2QmLQmmYqGwLs-removebg-preview.png"
-# path = "assets/rubber-duck-removebg-preview.png"
-# path = "/Users/mkbabb/Programming/fourier-animate/assets/IMG_7304.jpg"
-# path = "assets/smiley.png"
-# path = "/Users/mkbabb/Programming/fourier-animate/assets/Square_-_black_simple.svg.png"
-path = "assets/SpongeBob_SquarePants_character.svg.png"
-# path = "assets/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.png"
-# path = (
-#     "assets/Mona_Lisa__by_Leonardo_da_Vinci__from_C2RMF_retouched-removebg-preview.png"
-# )
-# path = "assets/SMASTitle.png"
-# path = "assets/super-mario-world---button-fin-1567640652381-removebg-preview.png"
-# path = "assets/375.jpg"
-# path = "assets/MV5BMGVlOGI0OTctZGZmOC00ZGE5LWE4YzUtOGVjZjVkYTcyY2Q3XkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_FMjpg_UX1000_.jpg"
-# path = "assets/71b6S5TlExL._SL1500_.jpg"
-# path = "assets/329527.jpg"
-# path = "assets/Eraserhead-and-Present-Mic.jpg"
-# path = "assets/deiywwr-0d9719ae-59b0-4fbc-adc3-a26de869d84e.png"
-# path = "assets/qrcode.png"
-# path = "assets/FO0S0IRWUAIF85e.png"
+def main(image_path: str, n: int = 50, plot: bool = False):
+    im = load_image(image_path)
 
-im = load_image(path)
+    height, width = im.shape
+    aspect_ratio = width / height
 
-height, width = im.shape
-aspect_ratio = width / height
+    # scale the image down; performance savings.
+    # INTER_AREA is good for down-scaling.
+    im = cv2.resize(
+        im, scale_to_aspect_ratio(n, aspect_ratio), interpolation=cv2.INTER_AREA
+    )
 
-n = 50
+    contours = find_contours(im)
+    all_points = np.vstack((*contours,))
+    all_points = np.unique(all_points, axis=0)
+    all_points = np.insert(all_points, 0, [0, 0], axis=0)
 
-im = cv2.resize(
-    im, scale_to_aspect_ratio(n, aspect_ratio), interpolation=cv2.INTER_AREA
-)
+    # Potential code for maximizing playground draw space.
+    # not feasible due to the poor N^2 implementation of order_points.
 
+    # all_points = [tuple(reversed(i)) for i in all_points]
+    # t_im = np.zeros(im.shape, dtype=np.uint8)
+    # for p in all_points:
+    #     t_im[p] = 1
 
-fig, ax = plt.subplots(figsize=(5 * aspect_ratio, 5))
+    N, M = scale_to_aspect_ratio(PLAYGROUND_SIZE, aspect_ratio)
 
+    # t_im = cv2.resize(t_im, (N, M), interpolation=cv2.INTER_AREA)
 
-contours = find_contours(im)
-all_points = np.vstack((*contours,))
-all_points = np.unique(all_points, axis=0)
-all_points = np.insert(all_points, 0, [0, 0], axis=0)
+    # all_points = np.transpose(np.nonzero(t_im))
 
-# all_points = [tuple(reversed(i)) for i in all_points]
+    all_points = all_points.astype(float)
 
-# t_im = np.zeros(im.shape, dtype=np.uint8)
-# for p in all_points:
-#     t_im[p] = 1
+    TSR = center(all_points) @ rotate(180) @ normalize(all_points) @ scale(-1 * N, M)
 
-N, M = scale_to_aspect_ratio(PLAYGROUND_SIZE, aspect_ratio)
-# t_im = cv2.resize(t_im, (N, M), interpolation=cv2.INTER_AREA)
+    all_points = pad_3d(all_points) @ TSR
+    all_points = to_2d(all_points)
 
-# all_points = np.transpose(np.nonzero(t_im))
-all_points = all_points.astype(float)
+    all_points, distances = order_points(all_points)
 
-TSR = center(all_points) @ rotate(180) @ normalize(all_points) @ scale(-1 * N, N)
+    code = generate_code(all_points, distances)
 
-all_points = pad_3d(all_points) @ TSR
-all_points = to_2d(all_points)
+    with open("./dist/generated.py", "w") as file:
+        file.write(code)
+
+    if plot:
+        fig, ax = plt.subplots(figsize=(5 * aspect_ratio, 5))
+
+        # plt.scatter(all_points[..., 0], all_points[..., 1], marker=".")
+        plt.plot(all_points[..., 0], all_points[..., 1], marker=".")
+        plt.show()
 
 
-all_points, distances = order_points(all_points)
+if __name__ == "__main__":
+    image_path = "assets/SpongeBob_SquarePants_character.svg.png"
+    n = 50
 
-# plt.scatter(all_points[..., 0], all_points[..., 1], marker=".")
-plt.plot(all_points[..., 0], all_points[..., 1], marker=".")
-plt.show()
-
-code = generate_code(all_points, distances)
-
-with open("./dist/generated.py", "w") as file:
-    file.write(code)
+    main(image_path=image_path, n=n)
